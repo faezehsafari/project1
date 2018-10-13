d2dbf1c75c31a9dbec0dce87306bdb9bc09a651f
faezehsafari <faezeh.hajisafari@gmail.com>
2018-10-13 11:27:19 -0400

    my first ever git commit

---
+++ aside.php
@@ -0,0 +1,23 @@
+<aside>
+		<form action="/action_page.php">
+			<fieldset>
+				<legend>Contact</legend>
+ 					 First name:<br>
+ 					 <input type="text" name="firstname" value="Mickey">
+  <br>
+  					Last name:<br>
+ 					 <input type="text" name="lastname" value="Mouse">
+  <br><br>
+  					<input type="radio" name="gender" value="male" checked> Male<br>
+  					<input type="radio" name="gender" value="female"> Female<br>
+  					<select name="country">
+					    <option value="USA">USA</option>
+					    <option value="Canad">Canad</option>
+					    <option value="Brasil">Brasil</option>
+					    <option value="Iran">Iran</option>
+					</select>
+  					<input type="submit" value="Submit">
+  			</fieldset>
+		</form> 
+	
+</aside>
